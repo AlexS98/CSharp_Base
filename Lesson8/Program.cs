70d202a659d578b7b35cdb327e2bfeee6a778ee8
Alexander <alexandersviridenko98@gmail.com>
2020-11-25 21:59:52 +0200

    lesson 8

---
+++ Lesson8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank("qwe", 1_000_000);
+            Customer customer = new Customer("qwe", "qwe", 123456789, new Address());
+
+            CommonCard card = bank.EmitCard("sav", customer);
+            customer.AddCards(new[] { card });
+            card.Deposit(150);
+            card.Withdraw(120);
+            Console.ReadKey();
+        }
+    }
+}
